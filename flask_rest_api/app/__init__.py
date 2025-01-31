105cce7118326fb8446ba41f0d4b262c45dd11d4
Kristian Cross <etamin1985@yahoo.com>
2025-01-31 18:31:02 +0000

    Add logic to enable CORS

--- flask_rest_api/app/__init__.py
+++ flask_rest_api/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from app.config import config
 from app.routes import resources_bp
 from app.auth import auth_bp
@@ -7,6 +8,10 @@ from app.auth import auth_bp
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    # Enable CORS
+    CORS(app, resources={r"/*":{"origins":
+    app.config["CORS_ORIGINS"]}})
 
     # Initialize JWT
     JWTManager(app)
