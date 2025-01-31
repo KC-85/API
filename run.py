567f2f0efc81809e062392d5d8842237ac5c075a
Kristian Cross <etamin1985@yahoo.com>
2025-01-31 19:25:15 +0000

    Add explicit module path settings

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Ensure the `app/` folder is in the system path
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 from app import create_app
 
 # Create an instance of the Flask app
