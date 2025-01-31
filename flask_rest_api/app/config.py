def556aedc9ac7df97314cefc032e6ff74ff34b3
Kristian Cross <etamin1985@yahoo.com>
2025-01-31 18:42:26 +0000

    Add optional debug setting & add cors origins

--- flask_rest_api/app/config.py
+++ flask_rest_api/app/config.py
@@ -7,3 +7,5 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "fallback_secret_key")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "fallback_jwt_secret")
+    DEBUG = os.getenv("DEBUG", "False").lower() in["true", "1"]
+    CORS_ORIGINS = os.getenv("CORS_ORIGINS", "*") # Allow all origins by default
