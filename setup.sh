6319f3600f6b63b5137be0e9c578536a44d48c32
Kristian D Cross <etamin1985@yahoo.com>
2025-02-03 12:44:14 +0000

    Add Python virtual environment

--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@ sudo apt update && sudo apt install -y redis-server python3-pip
 echo "🚀 Starting Redis server..."
 redis-server --daemonize yes  # Starts Redis in the background
 
+echo "📦 Setting up Python virtual environment..."
+python3 -m venv venv  # Create virtual environment
+source venv/bin/activate  # Activate it
+
 echo "📦 Installing Python dependencies..."
 pip install -r requirements.txt
 
